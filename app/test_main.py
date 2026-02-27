956d22b7e10c3de050bbc1ad9546f9ead26da8ea
Alexey Dergunov <dergunovalexey2000@gmail.com>
2026-02-27 11:40:58 +0700

    remake docker image with distroless

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,8 @@
 import asyncio
+import os
 
 from tests.load_fake_data import load_fake_data
 
 if __name__ == "__main__":
-    asyncio.run(load_fake_data())
+    if os.getenv("LOAD_FAKE_DATA"):
+        asyncio.run(load_fake_data())
